f62ebe77d801131d7f4963ba9b807c2770329c41
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2022-12-28 21:04:13 +0800

    Update quotes

--- js/data.js
+++ js/data.js
@@ -15,4 +15,28 @@ export const quote = [
         text: "This is our big mistake: to think we look forward to death. Most of death is already gone. Whatever time has passed is owned by death.",
         author: "Seneca",
     },
+    {
+        text: "We are more often frightened than hurt; and we suffer more in imagination than in reality.",
+        author: "Seneca",
+    },
+    {
+        text: "No person has the power to have everything they want, but it is in their power not to want what they don’t have, and to cheerfully put to good use what they do have.",
+        author: "Seneca",
+    },
+    {
+        text: "People are frugal in guarding their personal property; but as soon as it comes to squandering time they are most wasteful of the one thing in which it is right to be stingy.",
+        author: "Seneca",
+    },
+    {
+        text: "hink of the life you have lived until now as over and, as a dead man, see what’s left as a bonus and live it according to Nature. Love the hand that fate deals you and play it as your own, for what could be more fitting?",
+        author: "Marcus Aurelius",
+    },
+    {
+        text: "External things are not the problem. It’s your assessment of them. Which you can erase right now.",
+        author: "Marcus Aurelius",
+    },
+    {
+        text: "When we are no longer able to change a situation, we are challenged to change ourselves.",
+        author: "Viktor Frankl",
+    },
 ];
